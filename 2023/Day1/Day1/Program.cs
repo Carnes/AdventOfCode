e17f31f1c1a691d8c98cb2471c1162fb2d0fa40a
Sean Carnes <sean.carnes@venminder.com>
2023-12-01 17:13:07 -0500

    part 2

--- 2023/Day1/Day1/Program.cs
+++ 2023/Day1/Day1/Program.cs
@@ -1,13 +1,30 @@
 ﻿var inputs = await File.ReadAllLinesAsync("input.txt");
 var outputs = new List<string>();
 var total = 0;
+
+var stringNumbers = new List<string> { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", };
+
+static List<ResultSet> GetStringNumberIndexs(string stringNumber, string input, string number)
+{
+    var results = new List<ResultSet> { 
+        new ResultSet{Index = input.IndexOf(stringNumber), Value = number},
+        new ResultSet{Index = input.LastIndexOf(stringNumber), Value = number}
+    };
+    return results.Where(x => x.Index >= 0).DistinctBy(x=>x.Index).ToList();
+}
+
 foreach (var input in inputs)
 {
     var numbers = input.ToArray()
-        .Select(x => x.ToString())
-        .Where(x => int.TryParse(x.ToString(), out var y))
+        .Select((x, index) => new ResultSet { Value = x.ToString(), Index = index })
+        .Where(x => int.TryParse(x.Value, out var y))
         .ToList();
-    var result = numbers.First() + numbers.Last();
+
+    var stringNumbersFound = stringNumbers.SelectMany((x, index) => GetStringNumberIndexs(x, input, index.ToString()))
+        .ToList();
+
+    var results = numbers.Union(stringNumbersFound).OrderBy(o => o.Index);
+    var result = results.First().Value + results.Last().Value;
     outputs.Add(result);
     total += int.Parse(result);
     Console.WriteLine($"input: {input}, output: {result}");
@@ -15,3 +32,9 @@ foreach (var input in inputs)
 Console.WriteLine($"total: {total}");
 File.WriteAllLines("output.txt", outputs);
 Console.ReadKey();
+
+public class ResultSet
+{
+    public string Value { get; set; }    
+    public int Index { get; set; }
+}
