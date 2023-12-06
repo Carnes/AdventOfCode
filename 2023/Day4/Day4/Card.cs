5fda8908c7563c15b9e996338357d91ce8804f08
Carnes <whoismaha@gmail.com>
2023-12-05 22:29:37 -0500

    day4 part1

---
+++ 2023/Day4/Day4/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day4
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public List<int> WinningNumbers { get; set; }
+        public List<int> RandomNumbers { get; set; }
+        public List<int> MatchingNumbers => RandomNumbers.Join(WinningNumbers, r => r, w => w, (r, w) => r).ToList();
+        public int GetScore()
+        {
+            var mn = MatchingNumbers;
+            if (mn.Count() < 2) 
+                return mn.Count();
+            var score = 1;
+            for (var i = 1; i < mn.Count(); i++)
+                score *= 2;
+            return score;
+        }
+
+        public Card(string input)
+        {
+            var idAndData = input.Split(":");
+            Id = int.Parse(idAndData[0].Split(" ").Where(n => n.Any()).ToArray()[1]);
+            var winningNumbersAndPlayNumbers = idAndData[1].Split("|");
+            
+            WinningNumbers = winningNumbersAndPlayNumbers[0].Split(" ")
+                .Where(n=>n.Any())
+                .Select(n => int.Parse(n))
+                .ToList();
+            RandomNumbers = winningNumbersAndPlayNumbers[1].Split(" ")
+                .Where(n=>n.Any())
+                .Select(n => int.Parse(n))
+                .ToList();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Card {Id}: {GetScore()}");
+        }
+    }
+}
---
+++ 2023/Day4/Day4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            var cards = input.Select(c => new Card(c)).ToList();
+            var totalScore = cards.Sum(c => c.GetScore());
+            cards.ForEach(c=>c.Print());
+            Console.WriteLine($"Total: {totalScore}");
+        }
+    }
+}
