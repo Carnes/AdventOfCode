c320905155e1abe8e3d91d216cec28adf40b7a88
Carnes <whoismaha@gmail.com>
2024-12-08 22:01:14 -0500

    solution with sample, recursion fun!

---
+++ 2024/Day2/Day2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Day2;
+
+var input = await File.ReadAllLinesAsync("input_sample.txt");
+var numSafe = 0;
+
+foreach (var reportData in input)
+{
+    var r = new Report(reportData.Split(" ").Select(x=> Int32.Parse(x)).ToList());
+    r.Print();
+    if (r.IsSafe)
+        numSafe++;
+}
+
+Console.WriteLine($"Total safe: {numSafe}");
---
+++ 2024/Day2/Day2/Report.cs
@@ -0,0 +1,49 @@
+﻿namespace Day2;
+
+public class Report
+{
+    public List<int> Levels;
+    public bool? _isSafe = null;
+
+    public Report(List<int> levels)
+    {
+        Levels = levels;
+    }
+
+    public bool IsSafe
+    {
+        get
+        {
+            if(_isSafe == null)
+                _isSafe = CalcIsSafe(new Stack<int>(Levels));
+            return _isSafe.Value;
+        }
+    }
+
+    public void Print()
+    {
+        var prevColor = Console.ForegroundColor;
+        if(IsSafe) Console.ForegroundColor = ConsoleColor.Green;
+            
+        Console.Write($"IsSafe: {IsSafe}, ");
+        foreach (var level in Levels)
+            Console.Write($"{level} ");
+        
+        Console.WriteLine();
+        Console.ForegroundColor = prevColor;
+    }
+
+    private static bool CalcIsSafe(Stack<int> remainingLevels, bool? isAscending = null)
+    {
+        var current = remainingLevels.Pop();
+        var next = remainingLevels.Peek();
+        isAscending = isAscending ?? current < next;
+        if (isAscending == null) return false;
+        if (isAscending != current < next) return false;
+        if (current == next) return false;
+        if (Math.Abs(current - next) > 3) return false;
+        
+        if (remainingLevels.Count == 1) return true;
+        return CalcIsSafe(remainingLevels, isAscending);
+    }
+}
